e5ce5eb01a7498a66e1c0f43a621d7c1a52e837a
Philippe <kamokamophilippe13@gmail.com>
2023-07-30 19:37:27 -0500

    first commit

---
+++ src/main/java/com/javacode/hello/HelloController.java
@@ -0,0 +1,42 @@
+package com.javacode.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
+public class HelloController {
+
+    private List<String> names = new ArrayList<>();
+    private List<String> dates = new ArrayList<>();
+    
+    @GetMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("names", names);
+        model.addAttribute("dates", dates);
+        return "helloWorld";
+    }
+    
+    @PostMapping("/submit")
+    public String handleSubmit(@RequestParam String name, @RequestParam String date, Model model) {
+        names.add(name);
+        dates.add(date);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{index}")
+    public String handleDelete(@PathVariable int index, Model model ) {
+        if (index >= 0 && index < names.size() && index < dates.size() ) {
+            names.remove(index);
+            dates.remove(index);
+        }
+        return "redirect:/";
+    }
+}
+
